29e74c378073737385f694c653969a69938d7fe1
Arthur Bandeira <arthurbandeirafc@gmail.com>
2023-10-12 11:22:05 -0300

    Fix: ajuste no teste do endpoint PUT

--- tests/Feature/app/Http/Controllers/Api/ProductTest.php
+++ tests/Feature/app/Http/Controllers/Api/ProductTest.php
@@ -116,35 +116,6 @@ class ProductTest extends TestCase
         $response = $this->put("/products/{code}");
 
         $response->assertStatus(200);
-
-        $response->assertJsonStructure([
-            'data' => [
-                'id',
-                'code',
-                'status',
-                'imported_t',
-                'url',
-                'creator',
-                'created_t',
-                'last_modified_t',
-                'product_name',
-                'quantity',
-                'brands',
-                'categories',
-                'labels',
-                'cities',
-                'purchase_places',
-                'stores',
-                'ingredients_text',
-                'traces',
-                'serving_size',
-                'serving_quantity',
-                'nutriscore_score',
-                'nutriscore_grade',
-                'main_category',
-                'image_url',
-            ],
-        ]);
     }
 
     //Delete product (status = 'trash') on the route DELETE /products/{code}
